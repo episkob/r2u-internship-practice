f304e4e9002aaa8e0776c132105d413ee3476572
Iaroslav Tsymbaliuk <episkob@gmail.com>
2025-06-30 12:56:08 +0200

    feat: initial Maven project with Java 21 and Hello World entrypoint
    - Added basic Main.java with simple output
    - Configured pom.xml with Java 21 compatibility
    - Included maven-jar-plugin for executable JAR generation

---
+++ Java/Spring Basics-May 6-2025/src/main/java/it/r2u/Main.java
@@ -0,0 +1,11 @@
+package it.r2u;
+
+// Entry point of the application
+public class Main {
+
+    public static void main(String[] args) {
+        // Print a simple greeting to the console
+        System.out.println("Hello World!");
+    }
+
+}
